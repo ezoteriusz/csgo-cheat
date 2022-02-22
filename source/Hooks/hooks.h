2d26e5087c2caacca6c13fac2f3340e7bc54e122
Gracjan Niesporek <niesporek1234@gmail.com>
2022-02-22 12:45:45 +0100

    fixed missed library

--- source/Hooks/hooks.h
+++ source/Hooks/hooks.h
@@ -1,5 +1,5 @@
 #include <Windows.h>
-#include "../ext/minhook/MinHook.h"
+#include "../../ext/minhook/MinHook.h"
 
 namespace hooks {
 	inline unsigned int get_virtual(void* _class, unsigned int index) { return static_cast<unsigned int>((*static_cast<int**>(_class))[index]); }
